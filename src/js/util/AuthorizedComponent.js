78eb06d56865184920ad935657604f51d4b3e1de
lukmanlukmin <mochammad.lukman@ralali.com>
2017-09-13 11:48:04 +0700

    dirubah lagi ACL nya, masih error

--- src/js/util/AuthorizedComponent.js
+++ src/js/util/AuthorizedComponent.js
@@ -3,25 +3,30 @@ import axios from "axios";
 import { getAksesUser } from "../action/authAction"
 
 function initial(){
-  if(sessionStorage.getItem('auth-component')===undefined || sessionStorage.getItem('auth-component')===null) {
-    sessionStorage.setItem('auth-component', JSON.stringify({akses_komponen:undefined}))
+  if(sessionStorage.getItem('auth-component')===undefined || sessionStorage.getItem('auth-component')===null || sessionStorage.getItem('auth-component')==={}) {
+    sessionStorage.setItem('auth-component', JSON.stringify({ akses_komponen : { page : [] } }))
   }
-  return JSON.parse(sessionStorage.getItem('auth-component'))
-}
-
-const AuthorizedComponent = {
-  akses_komponen: initial().akses_komponen,
-  authComponent(komponen,cb) {
-    const classname = komponen.constructor.name
-    const pathname = komponen.props.location.pathname
-    const mappedPage= this.akses_komponen.page.reduce((init, ojmbt)=>{
+  const jembut=JSON.parse(sessionStorage.getItem('auth-component')).page.reduce((init, ojmbt)=>{
       init[ojmbt.path]=ojmbt.component.reduce((compinit, compObj)=>{
         compinit[compObj.name]=compObj
         return compinit
       },{})
       return init
     },{})
-    
+  return jembut
+}
+function getAuthComponent(akses_komponen) {
+  console.log('this.akses_komponen',akses_komponen)
+  if(akses_komponen===undefined)  return initial()
+  return akses_komponen
+}
+const AuthorizedComponent = {
+  akses_komponen: undefined,
+  authComponent(komponen,cb) {
+    const classname = komponen.constructor.name
+    const pathname = komponen.props.location.pathname
+    const mappedPage= getAuthComponent(this.akses_komponen)
+    console.log('mappedPage',mappedPage) 
     if(mappedPage[pathname]==undefined || mappedPage[pathname][classname]==undefined){
       // override all component mounting
       komponen.render = () => {
@@ -37,11 +42,15 @@ const AuthorizedComponent = {
     }
   },
   setAuthComponent(authAkses,cb) {
-    this.akses_komponen=authAkses
-    console.log('abcd',this.akses_komponen)
+    this.akses_komponen=authAkses.page.reduce((init, ojmbt)=>{
+      init[ojmbt.path]=ojmbt.component.reduce((compinit, compObj)=>{
+        compinit[compObj.name]=compObj
+        return compinit
+      },{})
+      return init
+    },{})
+    console.log(this.akses_komponen)
     sessionStorage.setItem('auth-component', JSON.stringify(this))
-    console.log('cek setter',this.akses_komponen)
-    console.log('cek session',sessionStorage.getItem('auth-component'))
   }
 }
 
